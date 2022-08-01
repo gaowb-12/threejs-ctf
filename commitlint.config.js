6d1458d3cc756ab99d89ec029ef5d3517ecc0ecc
gaoweibao <gaoweibao@eversec.cn>
2022-08-01 19:27:21 +0800

    docs: commitLint添加注释

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,23 @@
+/**
+* feat：新功能
+* update：更新某功能
+* fix：修补某功能的bug
+* refactor：重构某个功能
+* optimize: 优化构建工具或运行时性能
+* style：仅样式改动
+* docs：仅文档新增/改动
+* chore：构建过程或辅助工具的变动
+* revert：回滚
+* ci：持续集成
+* test：测试
+*/
 module.exports = {
     extends: [
       "@commitlint/config-conventional"
     ],
     rules: {
       'type-enum': [2, 'always', [
-        'feat', 'update', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'ci', 'build', 'test'
+        'feat', 'update', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'ci', 'optimize', 'test'
        ]],
       'type-case': [0],
       'type-empty': [0],
